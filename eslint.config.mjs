cd7e504ac63b16a80c557a589c71d4d53e0ce491
tarunkumarkushwaha <tkk.tarunkumar02@gmail.com>
2025-02-06 16:08:38 +0530

    fixes for deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
-export default [
-  {
-    ignores: ["node_modules/", "dist/", ".next/"],
-  },
-  {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-    },
-    extends: ["next/core-web-vitals"],
-  },
-];
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+export default eslintConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // basePath: '/api',
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
